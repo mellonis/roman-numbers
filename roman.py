11daf28d5f6852aea7b84eedd0c40c1d30725207
mellonis <mellonis@mellonis.ru>
2016-08-21 11:05:42 +0500

    refactoring

--- roman.py
+++ roman.py
@@ -1,27 +1,20 @@
 import re
 
-
 class Roman(int):
     __digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     __pattern = re.compile("^M*(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$")
 
-    def __new__(class_, value):
+    def __new__(cls, value):
         try:
             value = int(value)
         except ValueError:
             value = str(value).replace(' ', '').upper()
-            if class_.__pattern.match(value) == None or not set(class_.__digits) >= set(value):
+            if cls.__pattern.match(value) == None or not set(cls.__digits) >= set(value):
                 raise ValueError("Error!")
-            integers = list(map(class_.__digits.get, value))
+            integers = list(map(cls.__digits.get, value))
             value = sum(-n if n < max(integers[i:]) else n for i, n in enumerate(integers))
-            return super(Roman, class_).__new__(class_, value)
+            return super(Roman, cls).__new__(cls, value)
         else:
             if value < 0:
                 raise ValueError("Error!")
-            return super(Roman, class_).__new__(class_, value)
-
-
-print(Roman('0'))
-print(Roman('1'))
-print(Roman('MCMXL') + Roman('I'))
-#0rIX
+            return super(Roman, cls).__new__(cls, value)
--- test_roman.py
+++ test_roman.py
@@ -4,3 +4,5 @@ from roman import Roman
 def test_roman_init():
     assert Roman(5) == 'V'
     assert Roman('IV') == 'IV'
+    assert Roman('MCMXL') + Roman('I') == Roman('MCMXLI')
+    # 0rIX
